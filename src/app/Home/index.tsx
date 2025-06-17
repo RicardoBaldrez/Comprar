01b74fc77266f1344fa24ebe1fbea19067ed2c49
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2025-06-17 18:33:29 -0300

    feat: Add path alias

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -5,7 +5,7 @@ import { styles } from "./styles";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
     </View>
   );
 }
