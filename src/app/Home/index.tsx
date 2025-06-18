1b9610424b6a20298d4314b052bb2621b3763953
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2025-06-18 11:46:42 -0300

    feat: Add Input component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,12 +3,14 @@ import { View, Image } from 'react-native';
 import { styles } from './styles';
 import logo from '@/assets/logo.png';
 import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <Button title="Adicionar" />
+      <Input placeholder="O que você comprar?" />
+      <Button title="Entrar" />
     </View>
   );
 }
